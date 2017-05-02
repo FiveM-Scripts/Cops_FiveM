350f386b2491d2e941b473176c4dfe015b71b5dd
Teddy VALETTE <teddyche@hotmail.fr>
2017-05-02 19:39:36 +0200

    Add files via upload

---
+++ police.sql
@@ -0,0 +1,33 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gta5_gamemode_essential`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `police`
+--
+
+CREATE TABLE `police` (
+  `identifier` varchar(255) NOT NULL,
+  `rank` varchar(255) NOT NULL DEFAULT 'Recrue'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `police`
+--
+ALTER TABLE `police`
+  ADD PRIMARY KEY (`identifier`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
