5dbaacc499d70251e46527ca8feddf961bd1cfe2
ghermans <glhermans@gmail.com>
2017-12-15 02:45:24 +0100

    fixed typo

--- police/config/config.lua
+++ police/config/config.lua
@@ -15,7 +15,7 @@ config = {
 	useJobSystem = false, -- require job system
 	useWeashop = false, -- require es_weashop
 	
-	stationBlipsEnabled = true, -- switch between true of false to enable/disable blips for police stations
+	stationBlipsEnabled = true, -- switch between true or false to enable/disable blips for police stations
 	useCopWhitelist = true,
 	enableCheckPlate = false, --require garages
 	
