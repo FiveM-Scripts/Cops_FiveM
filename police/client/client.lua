f21ec3f7654074c9a06c0da0bf2ba0bcb9498083
Teddy VALETTE <teddy.valette@telecomnancy.eu>
2018-01-05 11:16:08 +0100

    allow two players forced into one vehicle

--- police/client/client.lua
+++ police/client/client.lua
@@ -23,20 +23,6 @@ anyMenuOpen = {
 	isActive = false
 }
 
---It isn't recommanded to use this array directly, please just use it in order to retrieve quickly the key code your are searching
---[[
-local Keys = {
-	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}]]
-
 local clockInStation = {
   {x=850.156677246094, y=-1283.92004394531, z=28.0047378540039}, -- La Mesa
   {x=457.956909179688, y=-992.72314453125, z=30.6895866394043}, -- Mission Row
@@ -259,7 +245,13 @@ AddEventHandler('police:forcedEnteringVeh', function(veh)
 		local _, _, _, _, vehicleHandle = GetRaycastResult(rayHandle)
 
 		if vehicleHandle ~= nil then
-			SetPedIntoVehicle(GetPlayerPed(-1), vehicleHandle, 1)
+			if(IsVehicleSeatFree(vehicleHandle, 1)) then
+				SetPedIntoVehicle(GetPlayerPed(-1), vehicleHandle, 1)
+			else 
+				if(IsVehicleSeatFree(vehicleHandle, 2)) then
+					SetPedIntoVehicle(GetPlayerPed(-1), vehicleHandle, 2)
+				end
+			end
 		end
 	end
 end)
