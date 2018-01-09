6214d06cf7e695930a9f7f94acb4b5145caa6af6
Glenn Hermans <glhermans@gmail.com>
2018-01-09 02:09:49 +0100

    Quick update

--- police/__resource.lua
+++ police/__resource.lua
@@ -3,7 +3,8 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 client_scripts {
   'config/config.lua',
   'client/i18n.lua',
-  'locales/en.lua',  
+  'locales/en.lua',
+  'locales/fr.lua',
   'client/client.lua',
   'client/cloackroom.lua',
   'client/menu.lua',
