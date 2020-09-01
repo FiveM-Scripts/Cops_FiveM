ad3ba313bf7fe10593475e71b82a9451227a2b60
Glenn Hermans <glhermans@gmail.com>
2020-09-01 12:11:39 +0200

    fixed #181

---
+++ police/fxmanifest.lua
@@ -0,0 +1,31 @@
+fx_version 'bodacious'
+game 'gta5'
+version '1.5'
+
+ui_page('client/html/index.html')
+
+files({
+    'client/html/index.html',
+    'client/html/js/script.js',
+    'client/html/css/style.css',
+    'client/html/img/background.png',
+    'client/html/img/arrows_upanddown.jpg',
+    'client/html/fonts/SignPainter-HouseScript.ttf'
+})
+
+client_scripts {
+  'client/i18n.lua',
+  'locales/en.lua',
+  'locales/fr.lua',
+  'locales/de.lua',
+  'config/cloackroom.lua',
+  'config/config.lua',
+  'config/objects.lua',
+  'config/vehicles.lua',
+  'config/weapons.lua',
+  'client/client.lua',
+  'client/cloackroom.lua',
+  'client/menu.lua',
+  'client/garage.lua',
+  'client/armory
+  }
